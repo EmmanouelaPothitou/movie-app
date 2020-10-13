2f5445dba2ab6a1e796c6c94e7b5fae82a7ae890
Emmanouela Pothitou <emmanouelapothitou@gmail.com>
2020-10-13 19:40:00 +0300

    Updates

---
+++ precache-manifest.16919735acf556fbdccccb4f2d809054.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24eb65f7eaf420e0a44cf17fd1f16db4",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "2c565755ac6e3af34765",
+    "url": "/movie-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1c52794c68bceec89e7e",
+    "url": "/movie-app/static/css/main.5e5167a4.chunk.css"
+  },
+  {
+    "revision": "2c565755ac6e3af34765",
+    "url": "/movie-app/static/js/2.ba4498fb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/movie-app/static/js/2.ba4498fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c52794c68bceec89e7e",
+    "url": "/movie-app/static/js/main.cd58b77c.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
